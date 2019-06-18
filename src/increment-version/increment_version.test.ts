80346a8e4049513e2bc7077c7bd87b1fe916f886
René Föhring <rene.foehring@5minds.de>
2019-06-18 13:09:47 +0200

    Fix linter warnings for tests

--- src/increment-version/increment_version.test.ts
+++ src/increment-version/increment_version.test.ts
@@ -14,34 +14,34 @@ v2.0.0-alpha2
 v2.0.0-alpha3
 v2.1.0-beta1`;
 
-describe('incrementVersion()', () => {
-  it('should return the incremented version for the first alpha build of a new version', () => {
+describe('incrementVersion()', (): void => {
+  it('should return the incremented version for the first alpha build of a new version', (): void => {
     assert.equal(incrementVersion('3.2.1', BRANCH_DEVELOP, GIT_TAG_LIST), '3.2.1-alpha1');
   });
-  it('should return the incremented version for unknown pre-version suffixes', () => {
+  it('should return the incremented version for unknown pre-version suffixes', (): void => {
     assert.equal(incrementVersion('3.2.1-asdf5', BRANCH_DEVELOP, GIT_TAG_LIST), '3.2.1-alpha1');
   });
-  it('should return the incremented version for subsequent alpha versions', () => {
+  it('should return the incremented version for subsequent alpha versions', (): void => {
     assert.equal(incrementVersion('1.2.1-alpha7', BRANCH_DEVELOP, GIT_TAG_LIST), '1.2.1-alpha11');
   });
 
-  it('should return the incremented version for the first beta build of a alpha version', () => {
+  it('should return the incremented version for the first beta build of a alpha version', (): void => {
     assert.equal(incrementVersion('3.2.1-alpha3', BRANCH_BETA, GIT_TAG_LIST), '3.2.1-beta1');
   });
-  it('should return the incremented version for subsequent beta versions', () => {
+  it('should return the incremented version for subsequent beta versions', (): void => {
     // v3.2.1-beta3 not in tag list, falling back to numbering found there
     assert.equal(incrementVersion('2.1.0-beta3', BRANCH_BETA, GIT_TAG_LIST), '2.1.0-beta2');
   });
-  it('should return the incremented version for the stable build of a alpha version', () => {
+  it('should return the incremented version for the stable build of a alpha version', (): void => {
     assert.equal(incrementVersion('3.2.1-alpha42', BRANCH_MASTER, GIT_TAG_LIST), '3.2.1');
   });
-  it('should return the incremented version for the stable build of a beta version', () => {
+  it('should return the incremented version for the stable build of a beta version', (): void => {
     assert.equal(incrementVersion('3.2.1-beta4', BRANCH_MASTER, GIT_TAG_LIST), '3.2.1');
   });
 });
 
-describe('findNextSuffixNumber()', () => {
-  it('should work', () => {
+describe('findNextSuffixNumber()', (): void => {
+  it('should work', (): void => {
     assert.equal(findNextSuffixNumber('1.2.1', BRANCH_DEVELOP, GIT_TAG_LIST), 11);
     assert.equal(findNextSuffixNumber('1.3.0', BRANCH_DEVELOP, GIT_TAG_LIST), 1);
     assert.equal(findNextSuffixNumber('2.0.0', BRANCH_DEVELOP, GIT_TAG_LIST), 4);
