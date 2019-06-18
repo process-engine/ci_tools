c42a33b4be936491bee2f71cefe749e992bb76b0
René Föhring <rene.foehring@5minds.de>
2019-06-18 13:44:15 +0200

    Refactor increment-version command

--- src/increment-version/index.ts
+++ src/increment-version/index.ts
@@ -1,6 +1,6 @@
 import { getPackageVersion } from './package_version';
 import { getGitBranch, getGitTagList, gitAdd, gitCommit, gitPush, gitPushTags, gitTag, isDirty } from './git';
-import { VERSION_INCREMENTER_BRANCHES, incrementVersion } from './increment_version';
+import { APPLICABLE_BRANCHES, incrementVersion } from './increment_version';
 import { sh } from './shell';
 
 const BADGE = '[increment-version]\t';
@@ -8,40 +8,31 @@ const BADGE = '[increment-version]\t';
 export async function run(...args): Promise<void> {
   const isDryRun = args.indexOf('--dry') !== -1;
   const isForced = args.indexOf('--force') !== -1;
+  const isDirtyAndNotForced = isDirty() && !isForced;
 
   const packageVersion = getPackageVersion();
   const branchName = getGitBranch();
   const gitTagList = getGitTagList();
 
   const nextVersion = incrementVersion(packageVersion, branchName, gitTagList);
+  const notOnApplicableBranch = nextVersion == null;
+  const nextCommitMessage = `Bump version to ${nextVersion}`;
+  const nextTag = `v${nextVersion}`;
 
-  console.log(`${BADGE}isDryRun:`, isDryRun);
-  console.log(`${BADGE}isForced:`, isForced);
-  console.log('');
-  console.log(`${BADGE}packageVersion:`, packageVersion);
-  console.log(`${BADGE}branchName:`, branchName);
-  console.log(`${BADGE}gitTagList:`);
-  gitTagList.split('\n').forEach((line: string): void => console.log(`${BADGE}  ${line}`));
-  console.log('');
+  printInfo(packageVersion, branchName, gitTagList, isDryRun, isForced);
 
-  if (nextVersion == null) {
-    console.error(`${BADGE}Aborting since we are not on one of these branches:`);
-    console.error(`${BADGE}  ${VERSION_INCREMENTER_BRANCHES.join(', ')}`);
+  if (isDirtyAndNotForced) {
+    const workdirState = sh('git status --porcelain --untracked-files=no').trim();
+
+    console.error(`${BADGE}Can not proceed due to dirty git workdir:`);
+    printMultiLineString(workdirState);
+
     process.exit(1);
   }
 
-  commitPushAndTagNextVersion(nextVersion, isForced, isDryRun);
-}
-
-function commitPushAndTagNextVersion(nextVersion: string, isForced: boolean, isDryRun: boolean): void {
-  const nextTag = `v${nextVersion}`;
-  const nextCommitMessage = `Bump version to ${nextVersion}`;
-
-  if (isDirty() && !isForced) {
-    const workdirState = sh('git status --porcelain --untracked-files=no').trim();
-
-    console.error(`${BADGE}Can not proceed due to dirty git workdir:\n\n${workdirState}`);
-
+  if (notOnApplicableBranch) {
+    console.error(`${BADGE}Aborting since we are not on one of these branches:`);
+    console.error(`${BADGE}  ${APPLICABLE_BRANCHES.join(', ')}`);
     process.exit(1);
   }
 
@@ -52,6 +43,31 @@ function commitPushAndTagNextVersion(nextVersion: string, isForced: boolean, isD
     return;
   }
 
+  commitPushAndTagNextVersion(nextVersion, nextTag, nextCommitMessage);
+}
+
+function printInfo(
+  packageVersion: string,
+  branchName: string,
+  gitTagList: string,
+  isDryRun: boolean,
+  isForced: boolean
+): void {
+  console.log(`${BADGE}isDryRun:`, isDryRun);
+  console.log(`${BADGE}isForced:`, isForced);
+  console.log('');
+  console.log(`${BADGE}packageVersion:`, packageVersion);
+  console.log(`${BADGE}branchName:`, branchName);
+  console.log(`${BADGE}gitTagList:`);
+  printMultiLineString(gitTagList);
+  console.log('');
+}
+
+function printMultiLineString(text: string): void {
+  text.split('\n').forEach((line: string): void => console.log(`${BADGE}  ${line}`));
+}
+
+function commitPushAndTagNextVersion(nextVersion: string, nextTag: string, nextCommitMessage: string): void {
   sh(`npm version ${nextVersion}`);
   gitAdd('package.json');
   gitCommit(nextCommitMessage);
