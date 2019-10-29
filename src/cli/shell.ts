1d7d9de4bc177e7f206f4697b814aa2b4e34ba61
Alex 😎 <alexander.kasten@5minds.de>
2019-10-29 15:02:26 +0100

    :bug: Exit process if shell execution failed

--- src/cli/shell.ts
+++ src/cli/shell.ts
@@ -9,6 +9,10 @@ export function sh(command: string): string {
 export function asyncSh(command: string): Promise<string> {
   return new Promise((resolve: Function, reject: Function): void => {
     shell.exec(`${command} 2>&1`, { silent: true, async: true }, (code, stdout, stderr): void => {
+      if (code === 1) {
+        console.log(stdout);
+        process.exit(1);
+      }
       return resolve(stdout);
     });
   });
