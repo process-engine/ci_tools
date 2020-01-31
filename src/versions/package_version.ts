49ff83402bc82e74c27ce61e07dd5052e414c806
Alex 😎 <alexander.kasten@5minds.de>
2020-01-31 09:35:04 +0100

    :sparkles: Add getMajorPackageVersion function

--- src/versions/package_version.ts
+++ src/versions/package_version.ts
@@ -1,10 +1,22 @@
 import * as fs from 'fs';
 
+const versionRegex: RegExp = /(\d+)\.(\d+).(\d+)/;
+
 export function getPackageVersion(): string {
   const rawdata = fs.readFileSync('package.json').toString();
   const packageJson = JSON.parse(rawdata);
 
   return packageJson.version;
+}
+
+export function getMajorPackageVersion(): string {
+  const rawdata = fs.readFileSync('package.json').toString();
+  const packageJson = JSON.parse(rawdata);
+
+  const regexResult: RegExpExecArray = versionRegex.exec(packageJson.version);
+  const majorVersion: string = regexResult[1];
+
+  return majorVersion;
 }
 
 export function getPackageVersionTag(): string {
