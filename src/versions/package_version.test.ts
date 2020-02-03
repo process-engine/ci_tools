7ccce3cc3e4b4007bf8058ae656f10b54812821e
Alex 😎 <alexander.kasten@5minds.de>
2020-02-03 11:47:03 +0100

    :sparkles: Add test for getMajorPackageVersion

--- src/versions/package_version.test.ts
+++ src/versions/package_version.test.ts
@@ -1,11 +1,19 @@
 import * as assert from 'assert';
-import { getPackageVersion, getPackageVersionTag } from './package_version';
+import { getMajorPackageVersion, getPackageVersion, getPackageVersionTag } from './package_version';
 
 describe('package_version.ts', (): void => {
   describe('getPackageVersion()', (): void => {
     it('should return something', (): void => {
       const packageVersion = getPackageVersion();
       const versionRegex = /\d+\.\d+\.\d+/;
+      assert.ok(packageVersion.match(versionRegex));
+    });
+  });
+
+  describe('getMajorPackageVersion()', (): void => {
+    it('should return something', (): void => {
+      const packageVersion = getMajorPackageVersion();
+      const versionRegex = /\d+/;
       assert.ok(packageVersion.match(versionRegex));
     });
   });
