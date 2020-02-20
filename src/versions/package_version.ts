c3b8654b7166d060125957d70ed906665e0fa9cf
Alex 😎 <alexander.kasten@5minds.de>
2020-02-20 10:38:46 +0100

    :sparkles: Add functions for package-version

--- src/versions/package_version.ts
+++ src/versions/package_version.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as parser from 'xml2json';
 
 const versionRegex: RegExp = /^(\d+)\.(\d+).(\d+)/;
 
@@ -10,12 +11,43 @@ export function getPackageVersion(): string {
 }
 
 export function getMajorPackageVersion(): string {
-  const regexResult: RegExpExecArray = versionRegex.exec(getPackageVersion());
+  return getMajorVersion(getPackageVersion());
+}
+
+export function getPackageVersionTag(): string {
+  return `v${getPackageVersion()}`;
+}
+
+export function getDotnetPackageVersion(filePath: string): string {
+  const version = JSON.parse(getJsonFromFile(filePath)).Project.PropertyGroup.Version;
+
+  return version;
+}
+
+export function getDotnetMajorPackageVersion(filePath: string): string {
+  const version = getDotnetPackageVersion(filePath);
+
+  return getMajorVersion(version);
+}
+
+function getMajorVersion(version: string): string {
+  const regexResult: RegExpExecArray = versionRegex.exec(version);
   const majorVersion: string = regexResult[1];
 
   return majorVersion;
 }
 
-export function getPackageVersionTag(): string {
-  return `v${getPackageVersion()}`;
+function getJsonFromFile(filePath: string): string {
+  const csproj = fs.readFileSync(filePath, { encoding: 'utf8' });
+
+  const jsonString = parser.toJson(csproj);
+
+  return jsonString;
+}
+
+export function setDotnetPackageVersion(version, filePath: string): void {
+  const csProj = fs.readFileSync(filePath, { encoding: 'utf8' });
+  const csProjWithNewVersion = csProj.replace(getDotnetPackageVersion(filePath).toString(), version);
+  fs.writeFileSync(filePath, csProjWithNewVersion);
+  console.log(`Version set to ${version}`);
 }
