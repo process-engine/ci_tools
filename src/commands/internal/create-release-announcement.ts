f929884a73f2a0f44ec9d73b879bbef8fc55e79b
Sebastian Griesa <sebastian.griesa@gmail.com>
2020-10-14 20:13:30 +0200

    Fix spacing and clean up

--- src/commands/internal/create-release-announcement.ts
+++ src/commands/internal/create-release-announcement.ts
@@ -30,14 +30,15 @@ export async function getReleaseAnnouncement(mode: string): Promise<string> {
   const nextVersionTag = getVersionTag(nextVersion);
 
   const releaseHeadline = `*${nextVersionTag} was released!*`;
+  const releaseTagLinkFooter = `*For a more detailed changelog have a look at:* http://github.com/${getCurrentRepoNameWithOwner()}/releases/tag/${nextVersionTag}`;
 
   if (startRef == null) {
 
     const changelogText = `
-    ${releaseHeadline}
+${releaseHeadline}
 
-    *For a more detailed changelog have a look at:* http://github.com/${getCurrentRepoNameWithOwner()}/releases/tag/${nextVersionTag}
-      `
+${releaseTagLinkFooter}
+  `
     .replace('`', "'")
     .trim();
 
@@ -93,7 +94,7 @@ The new version includes the following changes:
 
 ${mergedPullRequestsText}
 
-*For a more detailed changelog have a look at:* http://github.com/${getCurrentRepoNameWithOwner()}/releases/tag/${nextVersionTag}
+${releaseTagLinkFooter}
   `
     .replace('`', "'")
     .trim();
